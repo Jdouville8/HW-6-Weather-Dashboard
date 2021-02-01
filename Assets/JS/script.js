78fe9f871e40cb03fee89db737abecd8ecee4724
Justin Douville <jdouville8@gmail.com>
2021-02-01 00:44:24 -0500

    Updated styling and got closer to local Storage upon refresh

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,48 +1,62 @@
+$(document).ready(function () {
+  var location = 0;
+
+//   var storedCity = function () {
+//     if (localStorage.count) {
+//         location = localStorage.count.location;
+//     }
+//   }
 
 
-$(document).ready(function (){
-    
-    var weatherSearch = function (city) {
+  var weatherSearch = function (city) {
+    var apiKey = "10c09efbf0b22626ae2ec897c06f2ced";
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&APPID=" +
+      apiKey;
 
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(queryURL);
 
-        var apiKey = "10c09efbf0b22626ae2ec897c06f2ced"
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey
+      console.log(response);
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-
-    
-    console.log(queryURL);
-    
-    console.log(response);
-
-    var tempKel = response.main.temp
-    console.log(tempKel)
+      var tempKel = response.main.temp;
+      console.log(tempKel);
 
       var tempFar = ((tempKel - 273.15) * 1.8 + 32).toFixed(2);
 
-    $(".city").text("City: " + response.name)
-    $(".wind").text("Current wind speed in " + city + " is: " + response.wind.speed)
-    $(".humidity").text("Current humidity: " + response.main.humidity)
-    $(".temp").text("Current temperature in Fahrenheit is: " + tempFar)
-  });
+      $(".city").text("City: " + response.name);
+      $(".wind").text(
+        "Current wind speed in " + city + " is: " + response.wind.speed
+      );
+      $(".humidity").text("Current humidity: " + response.main.humidity);
+      $(".temp").text("Current temperature in Fahrenheit is: " + tempFar);
+    });
+  };
 
-};
+//   storedCity();
+  
+  weatherSearch("london");
 
-weatherSearch('london');
-
-$('#saveBtn').on('click', function() {
+  $("#saveBtn").on("click", function () {
     // retrieves id from save button
     event.preventDefault();
-    
-    var newCity = $('#search-input').val();
-    console.log(newCity)
-    // Stores value of input in local storage object
-    localStorage.setItem('location', newCity);
-    
-    weatherSearch(newCity);
-})
 
+    location++;
+
+    var newCity = $("#search-input").val();
+    console.log(newCity);
+    // Stores value of input in local storage object
+    localStorage.setItem("count", location);
+    localStorage.setItem("location" + location, newCity);
+
+    weatherSearch(newCity);
+  });
+
+
+  weatherSearch(localStorage.getItem("count"))
 });
